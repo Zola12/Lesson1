5295c990c4400e9f59e4c23dc482f1f158d4cdbb
Zola <peach@Tsogzolmaas-MacBook-Air.local>
2021-09-06 01:02:31 -0700

    Initial commit

---
+++ src/Hello.java
@@ -0,0 +1,34 @@
+public class Hello {
+
+    //int -> integer buyu buhel too
+    //double -> butarhai too
+    //char -> character buyu temdegt
+    //String -> collection of chars buyu ug, uguulber
+    //boolean -> true/false
+    //= -> assignment operator buyu suulgaj uguh uildel
+    public static void main(String[] args){
+        int x = 15;
+        double y = 15.5;
+        char ch = '1';
+        String word = "aefeage";
+        boolean check = true;
+
+        int a = 15;
+        int b = 20;
+        int sum = a + b;
+
+        double u = 15;
+        double u1 = 20;
+
+        //System.out.println("result is " + a+b);
+        //System.out.println("result is " + a/b);
+        //System.out.println("result is " + u/u1);
+
+        String oguulber = "hello";
+        oguulber = 7+10 + oguulber;
+        System.out.println(oguulber);
+        oguulber = oguulber + 7*10;
+        System.out.println(oguulber);
+
+    }
+}
